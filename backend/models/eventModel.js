1477aba0c4ed66e8f355197b9089387175dcf823
Adrizen89 <adrienberard@hotmail.fr>
2024-10-16 09:57:22 +0200

    Add function for add & translate data event in MongoDB

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose')
 
 const EventSchema = mongoose.Schema({
     name:String,
-    location:String
+    location:String,
+    attendees: [
+        {
+            firstName:String, 
+            lastName:String,
+            dateJoined:Date
+        }
+    ]
 });
 
 module.exports = mongoose.model("Event", EventSchema);
