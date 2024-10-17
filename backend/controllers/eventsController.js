9fbb2e8d24741e9e7a8dcf706929c55dad7e3b38
Adrizen89 <adrienberard@hotmail.fr>
2024-10-17 11:13:28 +0200

    Add view of update event

--- backend/controllers/eventsController.js
+++ backend/controllers/eventsController.js
@@ -17,5 +17,39 @@ const getEvents = async (req, res) => {
     }
 }
 
+//récup event choisi pour l'édition
+const renderEditEvent = async (req, res) => {
+    const eventId = req.params.id;
 
-module.exports = { getEvents };
+    try {
+        const mysqlConnection = await connectMySQL();
+        const [rows] = await mysqlConnection.execute('SELECT * FROM events WHERE id = ?', [eventId]);
+        await mysqlConnection.end();
+
+        if (rows.length > 0) {
+            const event = rows[0];
+            res.render('editEvent', { event }); // Affiche la vue d'édition avec les données de l'événement
+        } else {
+            res.status(404).send('Événement non trouvé');
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Erreur lors de la récupération de l\'événement' });
+    }
+};
+
+//modif de l'event sur mysql
+const updateEvent = async (req, res) => {
+    const { id, name, date_debut, date_fin, lieu, personnes_max } = req.body;
+
+    try {
+        const mysqlConnection = await connectMySQL();
+        await mysqlConnection.execute('CALL updateEvent(?, ?, ?, ?, ?, ?)', [id, name, date_debut, date_fin, lieu, personnes_max]);
+        await mysqlConnection.end();
+
+        res.redirect('/'); // Redirige vers la page d'accueil ou celle souhaitée après mise à jour
+    } catch (err) {
+        res.status(500).json({ message: 'Erreur lors de la mise à jour de l\'événement', error: err.message });
+    }
+};
+
+module.exports = { getEvents, renderEditEvent, updateEvent };
--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getEvents } = require('../controllers/eventsController');
+const { getEvents, renderEditEvent, updateEvent } = require('../controllers/eventsController');
 const router = express.Router();
 
 router.get('/events', getEvents);
+router.get('/events/edit/:id', renderEditEvent);
+router.post('/events/update', updateEvent);
 //router.post('/create', createUser);
 
 module.exports = router;
