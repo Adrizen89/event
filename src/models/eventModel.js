1802069b5dabc9ecfe3c04b5d7407257a5f1a70e
Adrizen89 <adrienberard@hotmail.fr>
2024-10-18 14:21:49 +0200

    Add JSON Schema

--- src/models/eventModel.js
+++ src/models/eventModel.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose')
 
 const EventSchema = mongoose.Schema({
-    name:String,
-    location:String,
-    date_start:Date,
-    date_finish: Date,
-    max_attendees : Number,
+    name:{type: String, required: [true, "'name' is required"],},
+    location:{type:String, required: [true, "'location' is required"],},
+    date_start:{type:Date, required: [true, "'date_start' is required"],},
+    date_finish: {type:Date, required: [true, "'date_finish' is required"],},
+    max_attendees : {type:Number, required: [false],},
     attendees: [
         {
-            firstName:String, 
-            lastName:String,
+            firstName:{type:String, required: [true, "'firstName' is required"],}, 
+            lastName:{type:String, required: [true, "'lastName' is required"],},
             dateJoined:{
                 type: Date,
-                default: null
+                default: null,
+                required: [false],
             }
         }
     ]
